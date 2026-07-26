24120ef83b1b66547ba5c2e278701c3af02ba5ff
Taha <tahawaqar159@gmail.com>
2026-07-26 13:09:49 +0500

    my first game using SFML

--- my first game/my first game.cpp
+++ my first game/my first game.cpp
@@ -5,8 +5,16 @@ int main()
     // initialize 
     sf::RenderWindow window(sf::VideoMode(1366, 768), "My First Game");
     sf::CircleShape shape(50.0f);
+	sf::RectangleShape rectangle(sf::Vector2f(120.0f, 60.0f));
     shape.setFillColor(sf::Color::Green);
-    shape.setPosition();
+    shape.setPosition(sf::Vector2f(100, 100));
+    shape.setOutlineThickness(10);
+    shape.setOutlineColor(sf::Color::Red);
+
+    rectangle.setFillColor(sf::Color::Blue);
+    rectangle.setPosition(sf::Vector2f(300, 300));
+    rectangle.setOutlineColor(sf::Color::Cyan);
+    rectangle.setOutlineThickness(15);
     // initialize
 
     while(window.isOpen())
@@ -24,7 +32,7 @@ int main()
         // draw any thing here 
         window.clear(sf::Color::Black);
         window.draw(shape);
-
+        window.draw(rectangle);
 		window.display();
         // draw any thing here 
     }
